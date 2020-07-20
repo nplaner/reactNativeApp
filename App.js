f5c434e32ba030fc2ee9ed5d1187c2578452ba46
Hang Xu <hangxu199371@gmail.com>
2020-07-20 13:50:19 -0400

    weather app finished

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { API_KEY } from './apiKey';
+import Weather from './components/Weather';
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [temperature, setTemperature] = useState(0);
+  const [weatherCondition, setWeatherCondition] = useState(null);
+
+  const fetchWeather = (lat = 25, lon = 25) => {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`,
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        setTemperature(json.main.temp);
+        setWeatherCondition(json.weather[0].main);
+        setIsLoading(false);
+        console.log(weatherCondition);
+      });
+  };
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      fetchWeather(position.coords.latitude, position.coords.longitude);
+    });
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {isLoading ? (
+        <View>
+          <Text>Weather App</Text>
+        </View>
+      ) : (
+        <Weather weather={weatherCondition} temperature={temperature} />
+      )}
       <StatusBar style="auto" />
     </View>
   );
---
+++ apiKey.js
@@ -0,0 +1,1 @@
+export const API_KEY = '2bd9145ce6784ddb2c86b27bbad11551';
